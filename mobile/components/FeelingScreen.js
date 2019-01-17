fa64269f6d2b5e49da956787bd59583915cb02d4
ryomaruo <r.maruo182@gmail.com>
2019-01-17 23:29:52 +0900

    update. 感情ボタンをrotate

--- mobile/components/FeelingScreen.js
+++ mobile/components/FeelingScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import { ImageBackground, View, Text, Image } from "react-native";
 import { Button } from "react-native-elements";
 import {
   Container,
@@ -18,50 +18,54 @@ export default class FeelingScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <View style={styles.circle_container}>
-          <Button
-            title="Tr"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[0], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="St"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[1], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="Co"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[2], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="An"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[3], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="Di"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[4], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="Sad"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[5], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="Wa"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[6], { backgroundColor: "#123456" })}
-          />
-          <Button
-            title="Wa"
-            titleStyle={{ fontSize: 4 }}
-            buttonStyle={Object.assign({}, styles.button, posi[7], { backgroundColor: "#123456" })}
-          />
+      <ImageBackground
+        source={require('../assets/nazuna.jpg')}
+        style={{width: '100%', height: '100%'}}>
+        <View style={styles.container}>
+          <View style={styles.circle_container}>
+            <Button
+              title="Relux"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[0], { backgroundColor: "green" })}
+            />
+            <Button
+              title="Cool"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[1], { backgroundColor: "skyblue" })}
+            />
+            <Button
+              title="Sad"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[2], { backgroundColor: "blue" })}
+            />
+            <Button
+              title="Bored"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[3], { backgroundColor: "purple" })}
+            />
+            <Button
+              title="Angry"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[4], { backgroundColor: "#f66" })}
+            />
+            <Button
+              title="Want"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[5], { backgroundColor: "orange" })}
+            />
+            <Button
+              title="Happy"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[6], { backgroundColor: "yellow" })}
+            />
+            <Button
+              title="Trust"
+              titleStyle={{ fontSize: 4 }}
+              buttonStyle={Object.assign({}, styles.button, posi[7], { backgroundColor: "lightgreen" })}
+            />
+          </View>
         </View>
-      </View>
+      </ImageBackground>
     );
   }
 }
@@ -70,7 +74,9 @@ const styles = {
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'black', 
+    opacity: 0.6, 
   },
   circle_container: {
     width: 200,
@@ -78,27 +84,31 @@ const styles = {
     position: 'relative'
   },
   button: {
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 40,
     borderColor: "transparent",
     borderWidth: 0,
-    borderRadius: 50,
-    position: 'absolute'
+    borderRadius: 40
   }
 }
 
 const btnCnt = 8,
       r = 100;
-let deg,rad;
+let deg,rad,rotate;
 let posi = [];
 for (let i = 0; i < btnCnt; i++) {
   deg = 360 / btnCnt * i;
   rad = deg * Math.PI / 180;
-  top = Math.sin(rad) * r + r - 25;  
-  left = Math.cos(rad) * r + r - 40;
+  top = Math.sin(rad) * r + r - 20;  
+  left = Math.cos(rad) * r + r - 55;
+  rotate = i*45 + 'deg';
   posi.push({
     top: top,
-    left: left
+    left: left,
+    position: 'absolute',
+    transform: [
+      { rotate: rotate }
+    ]
   });
 }
 
